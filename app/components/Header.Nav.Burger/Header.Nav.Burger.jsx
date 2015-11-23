0f11ee9aa58887ae128d9212c696e6e6f1d5d323
Noah Appel <noah.appel@lifelock.com>
2015-11-22 19:45:02 -0500

    Styles and animations for mobile nav. Added utils sass file.

--- app/components/Header.Nav.Burger/Header.Nav.Burger.jsx
+++ app/components/Header.Nav.Burger/Header.Nav.Burger.jsx
@@ -5,8 +5,8 @@ class Burger extends React.Component {
   render() {
     return (
       <div className="burger" onClick={this.props.toggleOpen}>
-        <div className="bun"></div>
-        <div className="bun"></div>
+        <div className="bun bun-top"></div>
+        <div className="bun bun-bottom"></div>
       </div>
     );
   }
