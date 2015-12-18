4f123295f80a749ca5fb0de718341f4397d99627
Noah Appel <noah@bop.fm>
2015-12-17 21:42:12 -0800

    Some layout tweaks.:

--- app/components/Page.ComingSoon/Page.ComingSoon.jsx
+++ app/components/Page.ComingSoon/Page.ComingSoon.jsx
@@ -7,11 +7,11 @@ class PageComingSoon extends React.Component {
       <div className="border-glitch"></div>
         <div className="col">
           <div className="center">
-            <img src="../../img/sr_idea1.png" alt="Sonny Ruckus" />
+            <img src="../img/sr_idea1.png" alt="Sonny Ruckus" />
             <h2>...coming soon</h2>
             <iframe
               width="100%"
-              height="450"
+              height="500"
               scrolling="no"
               frameBorder="no"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/236435663&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true">
@@ -24,11 +24,3 @@ class PageComingSoon extends React.Component {
 };
 
 export default PageComingSoon;
-
-// <iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/236435663&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-
-// <span className="color-glitch">o</span>
-// <span className="sonny">Sonny</span> Ruckus
-// <span className="bar">
-//   y Rucku
-// </span>
