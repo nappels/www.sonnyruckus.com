9ce7416f788b0c45d1d8a11d760be525d402d709
Noah Appel <noah@bop.fm>
2015-12-17 21:59:40 -0800

    Changing img tag to css bg

--- app/components/Page.ComingSoon/Page.ComingSoon.jsx
+++ app/components/Page.ComingSoon/Page.ComingSoon.jsx
@@ -7,7 +7,8 @@ class PageComingSoon extends React.Component {
       <div className="border-glitch"></div>
         <div className="col">
           <div className="center">
-            <img src="img/sr_idea1.png" alt="Sonny Ruckus" />
+            <div className="img-main"></div>
+
             <h2>...coming soon</h2>
             <iframe
               width="100%"
