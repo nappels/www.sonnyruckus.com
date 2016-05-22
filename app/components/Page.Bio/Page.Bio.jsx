00e89961a4764f50ea3280d781d4ae384d19166f
nappels <noah.appel@gmail.com>
2016-05-22 15:59:28 +0200

    Tweaked bio and added second track to music section.

--- app/components/Page.Bio/Page.Bio.jsx
+++ app/components/Page.Bio/Page.Bio.jsx
@@ -7,10 +7,20 @@ class PageBio extends React.Component {
         <div className="col">
           <h2>Bio</h2>
           <p>
-            In the foggy San Francisco summer of 2014, Juan Martinez met Noah Appel, and together the emcee, drummer duo started what would become Sonny Ruckus.
-            As the peieces started to come toether - Joe Ferrone on trumpet, Scott Mayhew on guitar, Clare Nauman on violin, Kaila Love emcee/vocals, and Colin Bjorklund on bass -
-            the sound started to form. Sonny Ruckus found it's indentity blending a fresh mix of Hip-Hop, Funk, and Dance, and is here to unleash it on you.
+          In the foggy San Francisco summer of 2014, Juan Martinez met Noah Appel, and together the emcee-drummer duo started what would become Sonny Ruckus.
+          As the pieces started to come together - Joe Ferrone on trumpet, Scott Mayhew on guitar, Clare Nauman on violin, Kaila Love emcee/vocals, and Colin Bjorklund on bass -
+          the sound started to form. Sonny Ruckus found its identity blending a fresh mix of Hip-Hop, Funk, and Dance, and is here to unleash it on you.
           </p>
+
+          <ul className="instrumentation">
+            <li>Juan Martinez - emcee</li>
+            <li>Kaila Love - emcee</li>
+            <li>Joe Ferrone - trumpet</li>
+            <li>Scott Mayhew - guitar</li>
+            <li>Clare Nauman - violin</li>
+            <li>Colin Bjorklund - bass</li>
+            <li>Noah Appel - drums</li>
+          </ul>
         </div>
       </section>
     );
--- app/components/Page.Music/Page.Music.jsx
+++ app/components/Page.Music/Page.Music.jsx
@@ -13,6 +13,14 @@ class PageMusic extends React.Component {
             frameBorder="no"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/236435663&amp;color=cb9326&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
           </iframe>
+
+          <iframe
+            width="100%"
+            height="166"
+            scrolling="no"
+            frameborder="no"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/238466798&amp;color=cb9326&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
+          </iframe>
         </div>
       </section>
     );
